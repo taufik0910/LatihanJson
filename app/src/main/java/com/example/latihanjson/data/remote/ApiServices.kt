d56da9ad456862c8e79aa4640647ffc9ad4a7960
taufik0910 <taufik.cradle@gmail.com>
2022-12-16 10:47:49 +0700

    revisi data json class

--- app/src/main/java/com/example/latihanjson/data/remote/ApiServices.kt
+++ app/src/main/java/com/example/latihanjson/data/remote/ApiServices.kt
@@ -1,9 +1,11 @@
 package com.example.latihanjson.data.remote
 
-import com.example.latihanjson.data.model.Article
+import com.example.latihanjson.data.model.NewsResponse
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
 
 interface ApiServices {
     @GET("/v2/top-headlines?country=us&apiKey=2f411b21be4e4de8ad57d91a07e0b791")
-    suspend fun getNews(): Article
+    suspend fun getNews(): NewsResponse
 }
--- app/src/main/java/com/example/latihanjson/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/latihanjson/ui/main/MainViewModel.kt
@@ -4,13 +4,13 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.latihanjson.data.model.ResponseDataNews
+import com.example.latihanjson.data.model.NewsResponse
 import com.example.latihanjson.data.remote.ApiClient
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
-    val resultNews = MutableLiveData<ResponseDataNews>()
+    val resultNews = MutableLiveData<NewsResponse>()
 
     fun fetchNews() {
         viewModelScope.launch(Dispatchers.IO)
